a2c0a7c79fbc538fb55569050e647490e9bd497d
Ilaria <ilaria.frandina@gmail.com>
2024-10-25 21:15:42 +0200

    [RSJ-22] aggiunta api generata da swagger per chiamare il back ed effettuare la login

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,10 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
+import { Configuration, UserControllerService } from './app/generated-api'; 
+
+const apiConfiguration = new Configuration({
+  basePath: 'http://localhost:7001/sti-jobs'
+});
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: Configuration,
+      useValue: apiConfiguration
+    },
+    UserControllerService
   ]
 }).catch(err => console.error(err));
