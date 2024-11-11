fd55d4a72d6cb9b3210b8017fcfc05ffd12cddd1
Spada ⚔️ <75368540+PierpaoloSpadafora@users.noreply.github.com>
2024-11-11 22:45:23 +0100

    fix

--- Backend/src/test/java/unical/demacs/rdm/StiJobsApplicationTests.java
+++ Backend/src/test/java/unical/demacs/rdm/StiJobsApplicationTests.java
@@ -5,12 +5,21 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import unical.demacs.rdm.controller.JobControllerTest;
+import unical.demacs.rdm.controller.MachineControllerTest;
 import unical.demacs.rdm.controller.ScheduleControllerTest;
+import unical.demacs.rdm.service.JobServiceImplTest;
+import unical.demacs.rdm.service.MachineServiceImplTest;
 import unical.demacs.rdm.service.ScheduleServiceImplTest;
 
 @Suite
 @SelectClasses({
+		JobControllerTest.class,
+		MachineControllerTest.class,
 		ScheduleControllerTest.class,
+
+		JobServiceImplTest.class,
+		MachineServiceImplTest.class,
 		ScheduleServiceImplTest.class
 })
 @SpringBootTest
